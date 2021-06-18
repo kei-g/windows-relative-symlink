85817e91822595a1b8bd2956d738defb36968dc7
kei-g <km.8k6ce+github@gmail.com>
2021-06-19 01:22:36 +0900

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# windows-relative-symlink
+
+This script converts windows shortcuts to relative symbolic links.
+
+## requirement
+
+Works on [PowerShell](https://github.com/PowerShell/PowerShell) 7 or later.  
+Requires promotion to administrator.
---
+++ make-shortcuts-relative.ps1
@@ -0,0 +1,56 @@
+$cwd = Get-Location
+$cwd = $cwd.Path.Split('\')
+$shell = New-Object -ComObject WScript.Shell
+Get-ChildItem -Recurse . | Where-Object { -not $_.PsIsContainer -and $_.FullName.EndsWith('.lnk') } | ForEach-Object {
+    $link = $shell.CreateShortcut($_.FullName)
+    if (-not $link.TargetPath) {
+        Clear-Variable link
+		Write-Output "${_.FullName} has no target path, so it has been omitted."
+        return
+    }
+    $from = $link.FullName.Split('\')
+    $path = @()
+    for ($i = $cwd.count; $i -lt $from.count - 1; $i++) {
+        $path += $from[$i]
+    }
+    $to = $link.TargetPath.Split('\')
+    if ($to[0] -ne $cwd[0]) {
+        $drive = '"' + $to[0] + '"'
+        Write-Output "${link.FullName} is linked to other drive ${drive}, so it has been omitted."
+        return
+    }
+    $relative = @()
+    for ($i = 0; $i -lt [Math]::Min($from.count, $to.count); $i++) {
+        if ($from[$i] -ne $to[$i]) {
+            for ($j = $i; $j -lt $from.count - 1; $j++) {
+                $relative += '..'
+            }
+            for ($j = $i; $j -lt $to.count; $j++) {
+                $relative += $to[$j]
+            }
+            break
+        }
+    }
+    $relative = $relative -join '\'
+    $path += $from[$from.count - 1] -creplace '.lnk', ''
+    $path = $path -join '\'
+    $item = Get-Item -ErrorAction SilentlyContinue $path
+    if ($item) {
+        Clear-Variable item
+        Write-Output "${path} already exists, so it has been omitted."
+        return
+    }
+    Clear-Variable item
+    $target = Get-Item $link.TargetPath
+    if ($target.PsIsContainer) {
+        Clear-Variable target
+        $path = '"' + $path + '"'
+        $relative = '"' + $relative + '"'
+        cmd /C "mklink /D ${path} ${relative}" *| Out-Null
+    }
+    else {
+        Clear-Variable target
+        New-Item -ItemType SymbolicLink -Name $path -Value $relative
+    }
+}
+Clear-Variable shell
